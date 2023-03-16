b37a0fdfe1ba7c212b782435807f3cacd7d06b48
21Choisungwon <spancho0810@naver.com>
2023-03-16 18:14:32 +0900

    수정

--- day02/2_prompt함수.js
+++ day02/2_prompt함수.js
@@ -6,6 +6,16 @@ console.log('2번 js파일')
 
 // console.log(alert(1, 3, 5));
 
+//사용자에게 prompt로 이름을 입력받고, 
+//어서오세요 홍길동님 출력하기
 
-confirm('진행 하시겠습니까?')
-console.log(confirm('진행'));
+let userName = prompt('이름을 입력하세요');
+
+document.write(' <h2>어서오세요 ',userName,' 님 </h2>');
+document.write('<p> ',userName,'의 등급은 VIP입니다.</p>')
+
+
+// 형식을 지정한 문자열 만들기
+// '' ,"" ,``
+// ``안에 ${} 를 사용하면 해당 부분은 문자열이라고 인식하지 않고 자바스크립트 명령어로 인식
+document.write(`<p>${userName}님의 등급은 VIP입니다.</p>`);
---
+++ day02/3_문자열.js
@@ -0,0 +1,24 @@
+console.log('3번 JS 파일');
+
+console.log('문자열 안의 작은 따옴표는 \'로 작성');
+console.log('문자열 안의 쌍따옴표는 \" 로 표시 ');
+
+console.log('안에다가는 " 사용');
+console.log("안에다가 ' 사용");
+console.log('엔터는 \n으로 표현 ');
+console.log('탭키는 \t로 표현');
+console.log('역슬래시는 \\로 표현');
+
+
+
+
+//문자열 객체 (string) 에서 사용할 수 있는 함수들
+//toUpperCase
+console.log('ABCdef'.toUpperCase());
+console.log('ABCdef'.toLowerCase());
+
+let msg = 'hello my Name is sungwon';
+console.log(msg.toUpperCase());
+console.log(msg.charAt(2));
+console.log(msg.length);
+console.log(msg.indexOf('N'));
