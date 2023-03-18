9f04eae53c48c2371e824a429cad0b2e7d8ea38b
21Choisungwon <spancho0810@naver.com>
2023-03-18 19:07:51 +0900

    수정

---
+++ day02/4_형변환.js
@@ -0,0 +1,23 @@
+console.log('4번 js 파일');
+
+console.log(typeof(Number('10')));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number('안녕하세요')); // NoN : 숫자가 아님을 나타내는 값(Number 타입);
+console.log(typeof String(15));
+console.log(String(true));
+console.log(String(false));
+
+//Boolean 타입으로 변환
+//falsy한 값 거짓같은 값
+//Boolean 타입으로 변환했을 때 false로 판단되는 값들
+
+console.log(Boolean(undefined));//false
+console.log(Boolean(null));//false
+console.log(Boolean(NaN));//false
+console.log(Boolean(''));//false
+console.log(Boolean(0));//false
+
+//trusy: 참 같은 값,
+console.log(Boolean('false'));// true
+console.log(Boolean('-1'));// true 0과 NaN을 제외한 숫자는 true
