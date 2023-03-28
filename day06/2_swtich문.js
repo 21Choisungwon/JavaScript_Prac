fb796846b1e3a5c6dd9d01e8e0505fe1ab80caca
21Choisungwon <spancho0810@naver.com>
2023-03-28 18:05:08 +0900

    수정본

---
+++ day06/2_swtich문.js
@@ -0,0 +1,18 @@
+
+console.log("2번 js 파일");
+
+
+let num = 10;
+
+switch(num){
+    case 100 : 
+        console.log("영역1");
+    case 50 :
+        console.log('영역2');
+    case 10 :
+        console.log('영역 3');
+    default:
+        console.log("잘못입력");
+}
+
+console.log("switch문 바깥쪽");
